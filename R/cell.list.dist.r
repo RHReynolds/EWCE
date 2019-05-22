82b66f81b5e920dffbecf8ad4aa0624ecb4f8122
RHReynolds <regina.reynolds.16@ucl.ac.uk>
2019-05-22 15:25:37 +0100

    Fix if temp is returned as a vector.

--- R/cell.list.dist.r
+++ R/cell.list.dist.r
@@ -3,10 +3,11 @@ cell.list.dist <- function(hitGenes,sct_data,annotLevel){
     ValidGenes = rownames(sct_data[[annotLevel]]$specificity)[rownames(sct_data[[annotLevel]]$specificity) %in% hitGenes]
     temp = sct_data[[annotLevel]]$specificity[ValidGenes,]
 
-    # If the function was based a single gene list... just return temp
-    #if(is.null(dim(hitGenes)[1])){
-    #    return(temp)
-    #}else{
+    # If the temp is returned as a vector
+    if(is.null(dim(temp)[1])){
+        return(temp)
+    }else{
+        # Otherwise rows across each column
         return(apply(temp,2,sum))
-    #}
+    }
 }
